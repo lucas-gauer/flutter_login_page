6329fb7b71a0c6c9680b4c94045a0e6498ed33c5
Lucas Gauer <lucasfgauer@gmail.com>
2021-12-03 06:52:30 -0300

    Create login page

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,72 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       appBar: AppBar(title: const Text('Login'),),
-      body: Container(),
+      body: Center(
+        child: SizedBox(
+          height: 256,
+          width: 512,
+          child: Card(
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+
+                  //* username
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      style: textTheme.headline6,
+                      decoration: const InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Usuário',
+                        hintText: 'usernamecriativo'
+                      ),
+                    ),
+                  ),
+
+                  //* password
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      style: textTheme.headline6,
+                      decoration: const InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Senha',
+                        hintText: 'senh@criativa123',
+                      ),
+                    ),
+                  ),
+
+                  //* button
+                  Expanded(
+                    child: Center(
+                      child: SizedBox(
+                        width: 128,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // TODO
+                          },
+                          child: Text(
+                            'Login',
+                            style: textTheme.headline6!.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
